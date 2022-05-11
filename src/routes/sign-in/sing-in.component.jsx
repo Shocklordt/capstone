ebca251de5246bd437aefab92116649cdb62cb38
shocklordt <arseny.sergeenko@gmail.com>
2022-05-11 15:25:17 +0300

    added sign-up form and generalized the button component

--- src/routes/sign-in/sing-in.component.jsx
+++ src/routes/sign-in/sing-in.component.jsx
@@ -1,12 +1,14 @@
 import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../utils/firebase/firbase.utils"
-
+import SignUpForm from "../../components/sign-up-form/sign-up-form.components"
 
 const SignIn = () => {
+
+
+
     const logGoogleUser = async () => {
         const { user } = await signInWithGooglePopup()
         const userDocRef = await createUserDocumentFromAuth(user)
     }
-
 
     return (
         <div>
@@ -14,6 +16,7 @@ const SignIn = () => {
             <button onClick={logGoogleUser}>
                 Sign in with Google Popup
             </button>
+            <SignUpForm></SignUpForm>
         </div>
     )
 }
--- src/utils/firebase/firbase.utils.js
+++ src/utils/firebase/firbase.utils.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app"
-import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider, } from "firebase/auth"
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword } from "firebase/auth"
 import { getFirestore, doc, getDoc, setDoc} from "firebase/firestore"
 
 // Your web app's Firebase configuration
@@ -13,20 +13,21 @@ const firebaseConfig = {
   };
   
   // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+  const firebaseApp = initializeApp(firebaseConfig);
 
-  const provider = new GoogleAuthProvider()
-  provider.setCustomParameters({
+  const googleProvider = new GoogleAuthProvider()
+  googleProvider.setCustomParameters({
       prompt: "select_account"
 
   })
 
   export const auth = getAuth()
-  export const signInWithGooglePopup = () => signInWithPopup(auth, provider)
-
+  export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider)
+  export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googleProvider)
   export const db = getFirestore()
 
-  export const createUserDocumentFromAuth = async (userAuth) => {
+  export const createUserDocumentFromAuth = async (userAuth, additionalInformation) => {
+    if (!userAuth) return;
     const userDocRef = doc(db, "users", userAuth.uid)
     const userSnapshot = await getDoc(userDocRef)
 
@@ -38,17 +39,18 @@ const firebaseConfig = {
             await  setDoc(userDocRef, {
                 displayName,
                 email,
-                createdAt
+                createdAt,
+                ...additionalInformation
             })
         } catch(error) {
             console.log("error creating the user", error.message)
         }
     }
     return userDocRef
-    // if user data does not exist
-    // create / set the document with the data from userAuth in my collection
+  }
 
-    
-    // check if user data exists
-    // return userDocRef
+  export const createAuthUserWithEmailAndPassword = async (email, password) => {
+    if(!email || !password) return;
+
+    createUserWithEmailAndPassword(auth, email, password)
   }
