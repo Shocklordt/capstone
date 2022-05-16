156de1e06b70151545e8cf70d5e30a7d468bbed8
shocklordt <arseny.sergeenko@gmail.com>
2022-05-16 22:19:47 +0300

    Added cart item placement/increment and styling

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,10 +1,15 @@
+import { useContext } from "react";
+import { CartContext } from "../../contexts/cart.context";
 import "./product-card.styles.scss"
 import Button from "../button/button.component"
 
 
 const ProductCard = ({ product }) => {
     const { name, price, imageUrl } = product;
-    
+    const { addItemToCart } = useContext(CartContext)
+
+    const addProductToCart = () => addItemToCart(product) 
+
     return (
         <div className="product-card-container">
             <img src={imageUrl} alt={`${name}`}></img>
@@ -14,7 +19,7 @@ const ProductCard = ({ product }) => {
                 <span className="price">{price}</span>
             </div>
             
-            <Button buttonType="inverted">Add to cart</Button>
+            <Button buttonType="inverted" onClick={addProductToCart}>Add to cart</Button>
         </div>
     )
 }
