2a95893bf69dc3546be1a95e6a32a237104bfb49
shocklordt <arseny.sergeenko@gmail.com>
2022-06-09 20:17:00 +0300

    created a different middleware solution

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,19 @@
 import { compose, createStore, applyMiddleware } from "redux";
-import logger from "redux-logger";
 import { rootReducer } from "./root-reducer";
+// import logger from "redux-logger";
 
-const middleWares = [process.env.NODE_ENV === "development" && logger].filter(
+const loggerMiddleware = (store) => (next) => (action) => {
+  if (!action.type) {
+    return next(action);
+  }
+
+  next(action);
+};
+
+/* const middleWares = [process.env.NODE_ENV === "development" && logger].filter(
   Boolean
-);
+); */
+const middleWares = [loggerMiddleware];
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
