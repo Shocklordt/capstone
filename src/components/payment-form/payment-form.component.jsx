b0f74677dcee9c841743474124a054601ab4f400
shocklordt <arseny.sergeenko@gmail.com>
2022-06-25 20:13:44 +0300

    added stripe integration to the checkout

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -13,11 +13,38 @@ const PaymentForm = () => {
     if (!stripe || !elements) {
       return;
     }
+    const response = await fetch("/.netlify/functions/create-payment-intent", {
+      methon: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ amount: 10000 }),
+    }).then((res) => {
+      return res.json();
+    });
+    const {
+      paymentIntent: { client_secret },
+    } = response;
+
+    const paymentResult = await stripe.confirmCardPayment(client_secret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+        billing_details: {
+          name: "Username",
+        },
+      },
+    });
+
+    if (paymentResult.error) {
+      alert(paymentResult.error);
+    } else if (paymentResult.paymentIntent.status === "succeeded") {
+      alert("payment successful");
+    }
   };
 
   return (
     <PaymentFormContainer>
-      <FormContainer>
+      <FormContainer onSubmit={paymentHandler}>
         <h2> Credit Card Payment: </h2>
         <CardElement />
         <Button buttonType={BUTTON_TYPE_CLASSES.inverted}> Pay now</Button>
--- src/components/payment-form/payment-form.styles.jsx
+++ src/components/payment-form/payment-form.styles.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const PaymentFormContainer = styled.div`
   height: 300px;
   display: flex;
-  flex-direction: center;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
 `;
